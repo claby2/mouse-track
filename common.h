cc639717677e00ed3122694f91622093397178e5
claby2 <wibow9770@gmail.com>
2020-06-19 18:01:17 +0800

    fixed undefined references with header

--- common.h
+++ common.h
@@ -1,10 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-extern const int WINDOW_WIDTH = 640;
-extern const int WINDOW_HEIGHT = 480;
-extern const int CURSOR_SIZE = 10;
-extern const int POLLING_RATE = 10; // Time between mouse position captures in milliseconds
+extern const int WINDOW_WIDTH;
+extern const int WINDOW_HEIGHT;
+extern const int CURSOR_SIZE;
+extern const int POLLING_RATE; // Time between mouse position captures in milliseconds
 extern int SCREEN_WIDTH;
 extern int SCREEN_HEIGHT;
 
--- source.cpp
+++ source.cpp
@@ -10,6 +10,12 @@
 #include "./common.h"
 #include "./heatmap.cpp"
 
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+const int CURSOR_SIZE = 10;
+const int POLLING_RATE = 10; // Time between mouse position captures in milliseconds
+int SCREEN_WIDTH;
+int SCREEN_HEIGHT;
 float SCALE_FACTOR_WIDTH;    // Equal to screen width divided by window width
 float SCALE_FACTOR_HEIGHT;   // Equal to screen height divided by window height
 
