b03b2ade263f4c76e657b4de7f93b07878c8a679
claby2 <wibow9770@gmail.com>
2020-06-19 18:38:59 +0800

    added renderer and window to common

--- common.h
+++ common.h
@@ -1,5 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
+#include <SDL2/SDL.h>
 
 extern const int WINDOW_WIDTH;
 extern const int WINDOW_HEIGHT;
@@ -7,5 +8,7 @@ extern const int CURSOR_SIZE;
 extern const int POLLING_RATE; // Time between mouse position captures in milliseconds
 extern int SCREEN_WIDTH;
 extern int SCREEN_HEIGHT;
+extern SDL_Window* gWindow;
+extern SDL_Renderer* gRenderer;
 
 #endif
